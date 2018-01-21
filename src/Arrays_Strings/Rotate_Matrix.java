990408edbd9e821ff12c6983834c763e35b350b0
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-01-21 14:22:14 -0800

    rotate

---
+++ src/Arrays_Strings/Rotate_Matrix.java
@@ -0,0 +1,24 @@
+package Arrays_Strings;
+
+public class Rotate_Matrix {
+
+    /*
+    Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+     */
+
+    void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length <= 1) return;
+        int len = matrix.length;
+        for (int i = 0; i < len/2; i++) { //len/2是offset，最多到1/2为起点
+            int right = len - 2 - i; //len - 2 - i 是这一行last second element，因为第一个要和最后一个做比较
+            for (int j = i; j <= right; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len - j - 1][i];
+                matrix[len - j - 1][i] = matrix[len - i - 1][len - j - 1];
+                matrix[len - i - 1][len - j - 1] = matrix[j][len - 1 - i];
+                matrix[j][len - 1 - i] = temp;
+            }
+        }
+    }
+
+}
