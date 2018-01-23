348e9401205af4120a0c9a1c3dccdba2a46a63c0
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-01-22 20:02:21 -0800

    KthToLast

---
+++ src/LinkedList/Return_Kth_to_Last.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+import PublicClass.ListNode;
+
+public class Return_Kth_to_Last {
+
+    /*
+    Implement an algorithm to find the kth to last element of a singly linked list.
+     */
+    /*
+    fast slow pointer, 1st loop fast k times, if list.len < k then just return head
+    else loop slow from beginning along with fast together, until fast == null, then slow is the result
+     */
+
+    public ListNode KthToLast(ListNode head, int k) {
+        ListNode fast = head;
+        while (k > 0) {
+            if (fast == null) return head;
+            fast = fast.next;
+            k--;
+        }
+        ListNode slow = head;
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+
+}
