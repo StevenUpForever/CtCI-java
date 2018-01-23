5e6d5cf44559fd964554bec9493695881027005b
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-01-22 19:40:17 -0800

    Remove_Dups

--- src/LinkedList/Remove_Dups.java
+++ src/LinkedList/Remove_Dups.java
@@ -1,8 +1,52 @@
 package LinkedList;
 
+import PublicClass.ListNode;
+
+import java.util.HashSet;
+
 public class Remove_Dups {
     /*
     Write code to remove duplicates from an unsorted linked list.
      */
+
+    /*
+    Use HashSet to filter the node
+     */
+    public ListNode removeDup(ListNode head) {
+        ListNode res = head;
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode pre = null;
+        while (head != null) {
+            if (!set.add(head)) {
+                pre.next = head.next;
+                head = null;
+            }
+            pre = head;
+            head = head.next;
+        }
+        return res;
+    }
+
+    /*
+    If cannot use buffer
+    for for loop the head, and remove dups
+     */
+    public ListNode removeDup2(ListNode head) {
+        ListNode res = head;
+        while (head != null) {
+            ListNode cur = head.next, pre = head;
+            while (cur != null) {
+                if (head.value == cur.value) {
+                    pre.next = cur.next;
+                    cur = null;
+                }
+                pre = cur;
+                cur = cur.next;
+            }
+            head = head.next;
+        }
+        return res;
+    }
+
 
 }
