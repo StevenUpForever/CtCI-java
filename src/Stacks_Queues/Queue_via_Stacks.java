11b99cc04cb114479f0e126f1163dc1805a1423b
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-01-23 21:38:16 -0800

    MyQueue

---
+++ src/Stacks_Queues/Queue_via_Stacks.java
@@ -0,0 +1,43 @@
+package Stacks_Queues;
+
+import java.util.Stack;
+
+public class Queue_via_Stacks {
+
+    /*
+    Implement a MyQueue class which implements a queue using two stacks.
+     */
+
+    class MyQueue {
+
+        Stack stack1;
+        Stack stack2;
+
+        public MyQueue() {
+            stack1 = new Stack();
+            stack2 = new Stack();
+        }
+
+        public void push(Object object) {
+            stack1.push(object);
+        }
+
+        public Object pop() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty())
+                    stack2.push(stack1.pop());
+            }
+            return stack2.pop();
+        }
+
+        public int size() {
+            return stack1.size() + stack2.size();
+        }
+
+        public boolean isEmpty() {
+            return stack1.isEmpty() && stack2.isEmpty();
+        }
+
+    }
+
+}
