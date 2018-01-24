b541264afe8e020be3abd711524bff047a6e2e33
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-01-23 23:36:38 -0800

    isBST

---
+++ src/Trees_Graphs/Validate_BST.java
@@ -0,0 +1,22 @@
+package Trees_Graphs;
+
+import PublicClass.TreeNode;
+
+public class Validate_BST {
+
+    /*
+    Implement a function to check if a binary tree is a binary search tree.
+     */
+
+    public boolean isBST(TreeNode root) {
+        return isBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+
+    }
+
+    private boolean isBSTHelper(TreeNode root, Long min, Long max) {
+        if (root == null) return true;
+        if (root.key <= min || root.key >= max) return false;
+        return isBSTHelper(root.left, min, (long)root.key) && isBSTHelper(root.right, (long)root.key, max);
+    }
+
+}
