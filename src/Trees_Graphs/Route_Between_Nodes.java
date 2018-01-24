a12d8b09534becf6ea4758bf65386aea93e607e9
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-01-23 22:48:12 -0800

    route

--- src/Trees_Graphs/Route_Between_Nodes.java
+++ src/Trees_Graphs/Route_Between_Nodes.java
@@ -1,11 +1,22 @@
 package Trees_Graphs;
 
+import PublicClass.GraphNode;
+
 public class Route_Between_Nodes {
 
     /*
     Given a directed graph, design an algorithm to  find out whether there is a route between two nodes.
      */
 
+    boolean route(GraphNode a, GraphNode b) {
+        if (a == null) return false;
+        if (a == b) return true;
+        boolean res = false;
+        for (GraphNode node: a.neighbors) {
+            res |= route(node, b);
+        }
+        return res;
+    }
 
 
 }
