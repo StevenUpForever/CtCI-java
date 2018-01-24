bc082ee9cbaf30e0f413d2b20591021f0cf71d29
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-01-23 22:52:32 -0800

    treeNode

---
+++ src/Trees_Graphs/Minimal_Tree.java
@@ -0,0 +1,24 @@
+package Trees_Graphs;
+
+
+public class Minimal_Tree {
+
+    /*
+    Given a sorted (increasing order) array with unique integer elements, write an algo­
+rithm to create a binary search tree with minimal height.
+     */
+
+    /*
+    Min height use mid value as current root, and recursively build left and right sub tree depends on current scope values
+     */
+
+    TreeNode buildBST(int[] array) {
+
+    }
+
+    TreeNode buildBSTHelper(int[] array, int left, int right) {
+        int mid = left + (right - left)/2;
+        TreeNode root = new
+    }
+
+}
