b24062486e5b719f340b48e4ed45de562db55a08
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-01-23 23:21:07 -0800

    createLists

---
+++ src/Trees_Graphs/List_of_Depths.java
@@ -0,0 +1,34 @@
+package Trees_Graphs;
+
+import PublicClass.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class List_of_Depths {
+
+    /*
+    Given a binary tree, design an algorithm which creates a linked list of all the nodes
+at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+     */
+    List<List> createLists(TreeNode root) {
+        List<List> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<TreeNode> list = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                list.add(poll);
+                if (poll.left != null) queue.offer(poll.left);
+                if (poll.right != null) queue.offer(poll.right);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+
+}
