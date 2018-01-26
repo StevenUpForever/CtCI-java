daac14a94ff1d9b511d1c624bf0d4003c8ae69e7
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-01-25 21:23:47 -0800

    route

--- src/PublicClass/GraphNode.java
+++ src/PublicClass/GraphNode.java
@@ -5,9 +5,11 @@ public class GraphNode {
 
     public int key;
     public List<GraphNode> neighbors;
+    public boolean visited;
     public GraphNode(int key) {
         this.key = key;
         this.neighbors = new ArrayList<GraphNode>();
+        visited = false;
     }
 
 }
