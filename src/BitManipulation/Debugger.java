997ac9f10dfe93c9116d32cab5e499197cc8a9a5
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-01 22:38:59 -0800

    Debugger

---
+++ src/BitManipulation/Debugger.java
@@ -0,0 +1,15 @@
+package BitManipulation;
+
+public class Debugger {
+
+    /*
+    Explain what the following code does: ((n & (n-1)) == 0).
+     */
+
+    /*
+    checking if n is multiple of 2
+    this means if n and n - 1 are all bits different
+    when n is multiple by 2, n will be only one bit of 1, and n - 1 will be all bit of 1 except the nth bit
+     */
+
+}
