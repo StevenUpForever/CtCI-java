3951b1a75eee8105277f3927b54e72d625544b06
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-01 22:11:52 -0800

    FlopBitToWin

---
+++ src/BitManipulation/BinaryToString.java
@@ -0,0 +1,10 @@
+package BitManipulation;
+
+public class BinaryToString {
+
+    /*
+    Given a real number between O and 1 (e.g., 0.72) that is passed in as a double, print the binary representation. If the number cannot be represented accurately in binary with at most 32 characters, print "ERROR:'
+     */
+
+
+}
---
+++ src/BitManipulation/FlipBitToWin.java
@@ -0,0 +1,20 @@
+package BitManipulation;
+
+public class FlipBitToWin {
+
+    /*
+    You have an integer and you can flip exactly one bit from a 0 to a 1. Write code to  nd the length of the longest sequence of ls you could create.
+EXAMPLE
+Input: 1775 (or: 11011101111) Output: 8
+     */
+
+    int FlopBitToWin(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+
+}
