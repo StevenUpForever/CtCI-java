8ce5bc790c7758599986c16f4f05376b5acfe286
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-02 00:26:31 -0800

    swap

--- src/BitManipulation/PairwiseSwap.java
+++ src/BitManipulation/PairwiseSwap.java
@@ -6,4 +6,11 @@ public class PairwiseSwap {
    possible (e.g., bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped, and so on).
      */
 
+    /*
+    use mask 1010101010 and 0101010101 AND x
+     */
+    int swap(int x) {
+        return (x & 0xaaaaaaaa >> 1) | (x & 0x55555555 << 1);
+    }
+
 }
