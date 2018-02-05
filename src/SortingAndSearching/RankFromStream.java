fd623284dd8069d98fb9d575372757ffe006f794
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-04 23:09:21 -0800

    PeaksAndValleys

---
+++ src/SortingAndSearching/RankFromStream.java
@@ -0,0 +1,15 @@
+package SortingAndSearching;
+
+public class RankFromStream {
+
+    /*
+    Imagineyouarereadinginastreamofintegers.Periodically,youwishtobeable to look up the rank of a numberx (the number of values less than or equal tox). lmplement the data structures and algorithms to support these operations. That is, implement the method track ( int
+x), which is called when each number is generated, and the method getRankOfNumber(int x), which returns the number of values less than or equal tox (not includingx itself).
+EXAMPLE
+Stream (in order of appearance): 5, getRankOfNumber(l) 0 getRankOfNumber(3) = 1 getRankOfNumber(4) 3 Hints:#301, #376, #392
+1, 4, 4,
+5, 9,
+7, 13, 3
+     */
+
+}
