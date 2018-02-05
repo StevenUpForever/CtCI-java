38540a1037814e83cdd529cd768d104318f8b21c
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-04 23:03:28 -0800

    SortedMatrixSearch

--- src/SortingAndSearching/SortBigFile.java
+++ src/SortingAndSearching/SortBigFile.java
@@ -6,4 +6,8 @@ public class SortBigFile {
     Imagine you have a 20 GB  le with one string per line. Explain how you would sort the file
      */
 
+    /*
+    Map reduce merge sort
+     */
+
 }
---
+++ src/SortingAndSearching/SortedMatrixSearch.java
@@ -0,0 +1,9 @@
+package SortingAndSearching;
+
+public class SortedMatrixSearch {
+
+    /*
+    Given an M x N matrix in which each row and each column is sorted in ascending order, write a method to  nd an element.
+     */
+
+}
