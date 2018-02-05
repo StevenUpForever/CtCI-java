fcebe1434ea5655590d1f006edfb33887ff78624
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-04 22:52:43 -0800

    SearchnRotatedArray

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.util.HashSet;
 import Arrays_Strings.URLify;
+import SortingAndSearching.SearchnRotatedArray;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Integer.parseInt(""));
+        SearchnRotatedArray obj = new SearchnRotatedArray();
+        System.out.println(obj.rotatedSearch(new int[]{16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}, 7));
     }
 }
