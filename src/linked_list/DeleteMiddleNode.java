d2f7724509095b0d12c618ce3bc799b198b002dd
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-25 19:02:33 -0800

    DeleteMiddleNode

--- src/linked_list/DeleteMiddleNode.java
+++ src/linked_list/DeleteMiddleNode.java
@@ -2,7 +2,7 @@ package linked_list;
 
 import PublicClass.ListNode;
 
-public class Delete_Middle_Node {
+public class DeleteMiddleNode {
 
     /*
     Implement an algorithm to delete a node in the middle (i.e., any node but the  rst and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
@@ -15,18 +15,17 @@ Result: nothing is returned, but the new linked list looks likea->b->d->e- >f
     slow fast pointers, fast go 2 steps, and slow go 1 step
      */
 
-    ListNode remove_mid(ListNode head) {
+    ListNode removeMid(ListNode head) {
         ListNode res = head;
         ListNode slow = head, fast = head;
         ListNode pre = null;
-        while (fast != null && fast.next != null) {
+        while (fast.next != null && fast.next.next != null) {
             pre = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
         if (slow != head && slow != null && slow.next != null) {
             pre.next = slow.next;
-            slow = null;
         }
         return res;
     }
