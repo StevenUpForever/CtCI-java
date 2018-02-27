eac032bc8cacf23edb641551d908534b8b5e8cc8
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-26 22:38:14 -0800

    Debugger

--- src/bit_manipulation/Debugger.java
+++ src/bit_manipulation/Debugger.java
@@ -9,6 +9,9 @@ public class Debugger {
     /*
     checking if n is multiple of 2
     this means if n and n - 1 are all bits different
+    when there's one bit is 1, -1 will be 0, and other bits keep the same, if it's 0, need to borrow 1 bit from
+    previous bit, then if -1 will be all bit diff, then every bit need to borrow one from previous, which means
+    every bit is 0, and there's 1 bit is 1, like 000100000 it's power of 2
     when n is multiple by 2, n will be only one bit of 1, and n - 1 will be all bit of 1 except the nth bit
      */
 
--- src/bit_manipulation/NextNumber.java
+++ src/bit_manipulation/NextNumber.java
@@ -6,6 +6,6 @@ public class NextNumber {
     Given a positive integer, print the next smallest and the next largest number that have the same number of 1 bits in their binary representation.
      */
 
-
+    //TODO: Solution
 
 }
