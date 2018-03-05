8020c196f8245359202649e21ae814308c7bd9eb
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-03-04 16:25:22 -0800

    TripleStep

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.HashSet;
 
 public class Main {
 
+    //TODO: 6. Math and Logic Puzzles
+
     public static void main(String[] args) {
         System.out.println(1);
     }
--- src/recursion_dp/TripleStep.java
+++ src/recursion_dp/TripleStep.java
@@ -4,17 +4,23 @@ public class TripleStep {
 
     /*
     A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs.
+
+     */
+
+    /*
+    Time: O(3^n)
+    Space: O(n)
      */
 
     public int TripleStep(int n) {
-        int res = 0;
+        int[] res = new int[]{0};
         tripStepHelper(n, 0, res);
-        return res;
+        return res[0];
     }
 
-    private void tripStepHelper(int n, int cur, int res) {
+    private void tripStepHelper(int n, int cur, int[] res) {
         if (cur >= n) {
-            res++;
+            res[0]++;
             return;
         }
         for (int i = 1; i <= 3; i++) {
@@ -22,14 +28,24 @@ public class TripleStep {
         }
     }
 
-    private int tripStepHelper2(int n, int cur, int res, int[] arr) {
+    /*
+    DP Solution:
+    Recursion是不知道下一步问题的答案，所以尝试所有下一步的可能
+    dp是assume知道了当前步骤的答案，是通过前几步推导出来的，dp[i] 表示到i时做多可能的步数，
+    当前步数 dp[i] = dp[i-3]+dp[i-2]+dp[i-1]
+    所以当寻找前三个答案的时候，如果找到，!=-1直接返回
+
+    Time: O(n)
+    Space: O(n)
+     */
+    private int tripStepHelper2(int n, int[] arr) {
         if (n < 0) return 0;
         if (n == 0) return 1;
-        if (arr[cur] != -1) return arr[cur];
+        if (arr[n] != -1) return arr[n];
         for (int i = 1; i <= 3; i++) {
-            arr[cur] += tripStepHelper2(n, cur + i, res, arr);
+            arr[n] += tripStepHelper2(n - i, arr);
         }
-        return arr[cur];
+        return arr[n];
     }
 
 }
