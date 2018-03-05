64915faa2ead8364baad69678ff7e7ea33756e95
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-03-04 23:44:17 -0800

    coins

--- src/recursion_dp/Coins.java
+++ src/recursion_dp/Coins.java
@@ -6,6 +6,11 @@ public class Coins {
     Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and pennies (1 cent), write code to calculate the number of ways of representing n cents.
      */
 
+    /*
+    Time: O(n^m) m is length of coins
+    Space: O(m)
+     */
+
     private int res = 0;
     public int coins(int[] coins, int n) {
         coinsHelper(coins, 0, n);
