7bc98300a0a4d0b882cff674b25c92218161b859
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-03-05 00:13:05 -0800

    SearchInRotatedArray

--- src/sorting_and_searching/SearchInRotatedArray.java
+++ src/sorting_and_searching/SearchInRotatedArray.java
@@ -1,6 +1,6 @@
 package sorting_and_searching;
 
-public class SearchnRotatedArray {
+public class SearchInRotatedArray {
 
     /*
     Given a sorted array of n integers that has been rotated an unknown number of times, write code to  nd an element in the array. You may assume that the array was originally sorted in increasing order.
