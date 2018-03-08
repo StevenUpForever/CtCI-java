7147d528b59404d688a57179304d827cc2c6ef98
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-03-07 22:24:10 -0800

    SparseSearch

--- src/sorting_and_searching/SparseSearch.java
+++ src/sorting_and_searching/SparseSearch.java
@@ -3,10 +3,10 @@ package sorting_and_searching;
 public class SparseSearch {
 
     /*
-    Given a sorted array of strings that is interspersed with empty strings, write a method to  nd the location of a given string.
+    Given a sorted array of strings that is interspersed with empty strings,
+    write a method to find the location of a given string.
     EXAMPLE "", '"', "",
-Input: ball,{"at", "ball"
-, "", "",}
+    Input: ball,{"at", "ball", "", "",}
      */
 
     /*
